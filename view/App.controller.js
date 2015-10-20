1148c38eb7136777c02f6990ff5d91875716b525
Andreas Wiedemann <andreas.wiedemann@sap.com>
2015-10-20 20:08:21 +0000

    Commit solution for section 2

--- view/App.controller.js
+++ view/App.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("com.sap.teched.view.app", {
+sap.ui.controller("com.sap.teched.view.App", {
 
 	/**
 	 * Called when a controller is instantiated and its View controls (if available) are already created.
 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-	 * @memberOf com.sap.teched.view.app
+	 * @memberOf com.sap.teched.view.App
 	 */
 	//	onInit: function() {
 	//
@@ -12,7 +12,7 @@ sap.ui.controller("com.sap.teched.view.app", {
 	/**
 	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 	 * (NOT before the first rendering! onInit() is used for that one!).
-	 * @memberOf com.sap.teched.view.app
+	 * @memberOf com.sap.teched.view.App
 	 */
 	//	onBeforeRendering: function() {
 	//
@@ -21,7 +21,7 @@ sap.ui.controller("com.sap.teched.view.app", {
 	/**
 	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * @memberOf com.sap.teched.view.app
+	 * @memberOf com.sap.teched.view.App
 	 */
 	//	onAfterRendering: function() {
 	//
@@ -29,7 +29,7 @@ sap.ui.controller("com.sap.teched.view.app", {
 
 	/**
 	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-	 * @memberOf com.sap.teched.view.app
+	 * @memberOf com.sap.teched.view.App
 	 */
 	//	onExit: function() {
 	//
